c42d0331766e722337c202fd2e04b76b7f5db594
abddulahaluosfy9876-glitch <abddulah.aluosfy9876@gmail.com>
2026-07-10 20:28:51 +0300

    Refactor settings.gradle.kts for plugin management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,17 @@
-// 1. إعلان الإصدارات فقط في جذر المشروع دون تطبيقها هنا
-plugins {
-    id("com.android.application") version "8.4.0" apply false
-    id("com.android.library") version "8.4.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
-}
-
+rootProject.name = "VoxPrompter"
+include(":app")
